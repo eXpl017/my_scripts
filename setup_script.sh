9d87403e2ed8c5d1b4e63c1926c0ca59d121256b
Snehal <singhsnehal.2k1@gmail.com>
2026-02-11 12:04:24 +0530

    Small untested changes made

--- setup_script.sh
+++ setup_script.sh
@@ -7,6 +7,7 @@ set -e
 
 # height and width of terminal window are stored in $LINES and $COLUMNS
 USER_HOME=$( getent passwd "${USER}" | cut -d: -f6 )
+USER_SHELL=$( getent passwd "${USER}" | cut -d: -f7 )
 BASHRC_PATH=${USER_HOME}/.bashrc
 VIM_CONFIG_PATH=${USER_HOME}/.vim
 GITHUB_KEYS_PATH=${USER_HOME}/.ssh/github_keys
@@ -16,7 +17,7 @@ GITHUB_KEYS_PATH=${USER_HOME}/.ssh/github_keys
 
 # to print a divider
 function print_div() {
-        printf '#%.0s' $(seq 1 $COLUMNS)
+    printf '#%.0s' $(seq 1 $COLUMNS)
 }
 
 # to check if command is installed on the system
@@ -24,24 +25,40 @@ function is_tool_installed() {
     command -v "$1" >/dev/null 2>&1
 }
 
+# set git configs
+function set_git_configs() {
+
+    echo "[+] Git Config Setup (globally)"
+
+    echo "[-] Github linked email must be entered if one wants their commits using it to be counted in the Contribution table as 'green'."
+    IFS= read -rp 'Enter username: ' git_name
+    IFS= read -rp 'Enter email: ' git_email
+
+    git config --global user.name ${git_name}
+    git config --global user.email ${git_email}
+    git config --global core.editor vim && echo "Set core git editor to vim"
+
+}
+
+
 # to setup git
 function git_setup() {
 
-    # set git configs
-    echo "[+] Setting git configs (globally)"
-    git config --global user.email $(IFS= read -p 'Enter email: ' && printf '%s' $REPLY)
-    git config --global user.name $(IFS= read -p 'Enter username: ' && printf '%s' $REPLY)
-    git config --global core.editor vim && echo "Set core git editor to vim"
+    if is_tool_installed "git"; then
+        echo "[+] Git present, proceeding"
+    else
+        echo "[+] Installing git..."
+        apt install -qq -y git-all
+    fi
+
+    set_git_configs
 
     # check if dir for github key already exists, if not, create it
     echo "[+] Creating directory for github keys if not present..."
-    if [[ ! -d $GITHUB_KEYS_PATH ]]; then
+    if [[ ! -d "$GITHUB_KEYS_PATH" ]]; then
         echo "[-] $GITHUB_KEYS_PATH doesn't exist, creating..."
         mkdir $GITHUB_KEYS_PATH
         echo "[-] Done"
-    else
-        echo "[-] Directory present, proceeding..."
-        return 0
     fi
 
     # ssh-keygen ecdsa keys (rsa and dsa are old algos - can use rsa but try to avoid dsa as much as possible)
@@ -53,13 +70,13 @@ function git_setup() {
         echo "[-] Keys exist, proceeding..."
     fi
 
-    # copy github public ssh key to clipboard
-    xclip -sel clip ${GITHUB_EYS_PATH}/github.pub
-    echo "[+] Copied public key to clipboard."
+    # displaying cmd to copy github public ssh key to clipboard
+    echo "[+] Use below command to copy key to clipboard, and then can manually add to Github"
+    echo "xclip -sel clip ${GITHUB_EYS_PATH}/github.pub"
 
     # add entry to ssh config
-    echo "Adding Github host config to ssh config."
-    echo -e "Host github.com\n\tHostName github.com\n\tIdentityFile ${GITHUB_KEYS_PATH}/github" >> ~/.ssh/config
+    echo "[+] Adding Github host config to ssh config."
+    echo -e "Host github-personal\n\tHostName github.com\n\tIdentityFile ${GITHUB_KEYS_PATH}/github" >> ~/.ssh/config
 
 }
 
@@ -67,22 +84,8 @@ function vim_setup() {
 
     echo "Setting up Vim"
 
-    if ! is_tool_installed "git"; then
-        echo "Git not available, exiting"
-        return 1
-    else
-        echo "Found git, proceeding!"
-    fi
-
-    if is_tool_installed "vim"; then
-        echo "Found vim, proceeding with setup."
-    else
-        echo "Vim not available, installing..."
-        apt-get install -y -qq vim
-    fi
-
     # backup current vim config, create if not present
-    if [[ -d ${VIM_CONFG_PATH} ]]; then
+    if [[ -d ${VIM_CONFIG_PATH} ]]; then
         echo "Backing up current vim config"
         mv ${VIM_CONFIG_PATH} ${USER_HOME}/.vim.bak.$(date +%Y-%m-%d_%H-%M-%S)
     fi
@@ -91,9 +94,7 @@ function vim_setup() {
     mkdir ${VIM_CONFIG_PATH}
     declare -a req_dirs=(autoload plugged colors backup)
     for dir_ in ${req_dirs[@]}; do
-        if [[ ! -d "${VIM_CONFIG_PATH}/${dir_}" ]]; then
-            mkdir "${VIM_CONFIG_PATH}/${dir_}"
-        fi
+        mkdir "${VIM_CONFIG_PATH}/${dir_}"
     done
     echo "Created required directories under ${VIM_CONFIG_PATH}"
 
@@ -114,6 +115,15 @@ function vim_setup() {
 
 ##### SCRIPT BEGIN #####
 
+
+# current user shell
+# exit if not bash
+# echo "User login shell: ${USER_SHELL}"
+# if [[ ${USER_SHELL} -ne $( which bash ) ]]; then
+#     echo "User not using bash...exiting"
+#     exit 1
+# fi
+# echo
 
 # current user info
 echo -e "[+] Current user info:\n$(id)"
@@ -147,10 +157,22 @@ done
 echo "[+] Done with installing tools."
 echo
 
+echo "[+] Performing re-check to see if tools installed"
+for tool in "${tools_to_install[@]}"
+do
+    if is_tool_installed "$tool"; then
+        continue
+    else
+        echo "[-] $tool not installed."
+    fi
+    print_div
+done
+echo "[+] Done"
+
 print_div
 
 git_setup
-# vim_setup
+vim_setup
 
 ##### BASHRC CHANGES #####
 
